7c294325d63552c7542755345c5112952ae6084d
Hildemberg Eling <122825060+Hildemberg986@users.noreply.github.com>
2023-06-22 19:43:34 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,23 @@
+fim = 0
+while fim != '0':
+    clear()
+    print('==================================')
+    print('========  MENU PRINCIPAL  ========')
+    print('==================================')
+    print('   1 - CADASTRAR CLIENTE')
+    print('   2 - PESQUISAR CLIENTE')
+    print('   3 - MODIFICAR CLIENTE')
+    print('   4 - DELETAR CLIENTE')
+    print('   0 - SAIR')
+    option = input('Escolha sua opção ? ')
+    if option == '1'
+        print ('opção 1')
+    elif option == '2':
+        print ('opção 2')
+    elif option == '3':
+        print('opçao 3')
+    elif option == '4':
+        print ('opção 4')
+    elif option == '0':
+        print ('Programa encerrado!')
+        break
