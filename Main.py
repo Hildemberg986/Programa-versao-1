358bb87987387e8c5f88bbdc826deabfefb0429e
Hildemberg Eling <122825060+Hildemberg986@users.noreply.github.com>
2023-06-22 19:46:58 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,12 @@
 fim = 0
+def clear():
+    import os
+    import platform
+    desk = platform.system()
+    if desk == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
 while fim != '0':
     clear()
     print('==================================')
@@ -11,13 +19,18 @@ while fim != '0':
     print('   0 - SAIR')
     option = input('Escolha sua opção ? ')
     if option == '1'
+        clear()
         print ('opção 1')
     elif option == '2':
+        clear()
         print ('opção 2')
     elif option == '3':
+        clear()
         print('opçao 3')
     elif option == '4':
+        clear()
         print ('opção 4')
     elif option == '0':
+        clear()
         print ('Programa encerrado!')
         break
